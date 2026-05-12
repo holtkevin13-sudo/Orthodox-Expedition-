7964bc90b4102cbb9379beab1ec48232cba91b54
holtkevin13-sudo <holt.kevin13@gmail.com>
2026-05-11 20:04:03 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -340,7 +340,7 @@
 // v4: added week.html, prayers.html, day-state, pause-card, prayers, and config JSON
 // Version bump forces cache clear and fresh install
 
-const CACHE_NAME = 'orthodox-expedition-v32';
+const CACHE_NAME = 'orthodox-expedition-v33';
 const STATIC_ASSETS = [
   '/Orthodox-Expedition-/',
   '/Orthodox-Expedition-/index.html',
